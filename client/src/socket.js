1d4e34ce4d634a987b96f1a482901e8452749d76
porezzz <jakub.marsz.04@gmail.com>
2023-12-24 23:37:01 +0100

    fixing port and cors issues

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-
-export const socket = io('http://localhost:3000')
+export const PORT = process.env.PORT || 3000;
+export const socket = io(`http://localhost:${PORT}`)
 
 
--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
-
+const cors = require('cors');
+const { PORT } = require("../client/src/socket");
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 app.get("/", (req, res) => {
   app.use(express.static('../client/dist'));
   res.sendFile(join(__dirname, "../client/dist/index.html"));
